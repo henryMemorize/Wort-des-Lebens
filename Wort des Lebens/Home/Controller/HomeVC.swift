f61e79072d86863c78a798cb010ee677f69cc885
henryMemorize <heinrichmerk@gmx.de>
2018-05-01 20:13:15 +0200

    sorting files in folders and deleting the storyboard

---
+++ Wort des Lebens/Home/Controller/HomeVC.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Wort des Lebens
+//
+//  Created by Henry on 30.04.18.
+//  Copyright © 2018 henryMemorize. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+}
+
